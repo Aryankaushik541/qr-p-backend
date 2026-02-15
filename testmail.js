5dbad43e245f5d4595acfffa6bb621e475e08929
Aryan Kaushik <aryankaushik541@gmail.com>
2026-02-15 21:56:33 +0530

    changes

---
+++ testmail.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const { sendMail } = require("./src/utils/mailer");
+
+sendMail({
+    to: process.env.EMAIL_USER,
+    subject: "Test Email",
+    text: "Testing email system"
+})
+    .then(() => console.log("Mail sent"))
+    .catch(err => console.error("Error:", err.message));
